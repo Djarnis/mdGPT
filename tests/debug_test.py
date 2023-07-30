03830bb7acf734c00b84e941a6c3e5e0291f7621
Jeppe Bårris <jeppe@redpitt.com>
2023-07-31 00:21:23 +0200

    :bug: Add debug test

---
+++ tests/debug_test.py
@@ -0,0 +1,16 @@
+import pytest
+from pathlib import Path
+from mdgpt import cli
+
+
+@pytest.fixture
+def cli_args():
+    return [
+        'debug',
+        'tests/prompts',
+    ]
+
+
+def test_debug(cli_args, monkeypatch):
+    monkeypatch.setattr('sys.argv', ['prog_name'] + cli_args)
+    cli()
